51e4cf5b30e853a1aaec7b00eba008bc12d5291b
rameenkhan20 <kh.rameen4@gmail.com>
2026-04-26 22:48:12 +0400

    Implement sign-up form layout in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Sign Up</Text>
+      <TextInput style={styles.input}
+        placeholder="Username"
+      ></TextInput>
+      <TextInput style={styles.input}
+        placeholder="Password"
+      ></TextInput>
+      <TextInput style={styles.input}
+        placeholder="Confirm Password"
+      ></TextInput>
+      <Pressable> 
+        <Text>Sign Up</Text>
+      </Pressable>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    margin: 20
+  },
+  input: {
+    height: 30,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 10
+  },
+  heading: {
+    fontSize: 26,
+    textAlign: "center",
+    margin: 20
+  }
+})
